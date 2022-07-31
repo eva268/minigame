b3e91afc2394b02ef4ce3c88126eb6266cf81c1d
eva268 <leva157@naver.com>
2022-07-31 22:03:07 +0900

    왼쪽 시프트 추가, 2개 이상만 pop 가능하도록 수정.

--- block.js
+++ block.js
@@ -1,14 +1,31 @@
 let blockColors = ["red", "blue", "green"];
 
 class block {
-  constructor(idx) {
-    this.idx = idx;
-    this.color = blockColors[int(random(0, blockColors.length))];
-    this.width = width / gridCol;
-    this.height = height / gridRow;
-    this.x = this.width * (this.idx % gridCol);
-    this.y = this.height * int(this.idx / gridCol);
+  constructor(i, j) {
+    this.col = i;
+    this.row = j;
+
+    this.width = width / col;
+    this.height = height / row;
+
+    this.x = this.width * (this.col);
+    this.y = height - this.height * (this.row + 1);
+
     this.poped = false;
+    this.color = blockColors[int(random(0, blockColors.length))];
+  }
+
+  posUpdate() {
+
+    this.x = this.width * (this.col);
+    this.y = height - this.height * (this.row + 1);
+  }
+
+  resize() {
+    this.width = width / col;
+    this.height = height / row;
+    this.x = this.width * this.col;
+    this.y = this.height * this.row;
   }
 
   clicked() {
@@ -18,30 +35,78 @@ class block {
       this.y < mouseY &&
       mouseY < this.y + this.height
     ) {
-      return this.idx;
-    } else return -1;
+      this.pop();
+    }
   }
 
-  fall() {
-    while (this.idx + gridCol < gridTotal && blocks[this.idx + gridCol] == 0) {
-      let temp = this.idx;
-      blocks[this.idx + gridCol] = blocks[this.idx];
-      blocks[this.idx] = 0;
-      this.idx = this.idx + gridCol;
-      this.x = this.width * (this.idx % gridCol);
-      this.y = this.height * int(this.idx / gridCol);
+  pop(cnt = 0) {
+    this.poped = true;
+    let blk;
+
+    blk = this.getBlock('L');
+    if (blk != undefined) {
+      if (this.color == blk.color) {
+        if (blk.poped == false) blk.pop();
+      } else cnt++;
+    } else cnt++;
+
+    blk = this.getBlock('R');
+    if (blk != undefined) {
+      if (this.color == blk.color) {
+        if (blk.poped == false) blk.pop();
+      } else cnt++;
+    } else cnt++;
+
+    blk = this.getBlock('U');
+    if (blk != undefined) {
+      if (this.color == blk.color) {
+        if (blk.poped == false) blk.pop();
+      } else cnt++;
+    } else cnt++;
+
+    blk = this.getBlock('D');
+    if (blk != undefined) {
+      if (this.color == blk.color) {
+        if (blk.poped == false) blk.pop();
+      } else cnt++;
+    } else cnt++;
+
+    if (cnt == 4) this.poped = false;
+    else blocks[this.col][this.row] = undefined;
+
+  }
+
+  getBlock(direction) {
+    let blk;
+    switch (direction) {
+      case 'L':
+        blk = blocks[this.col - 1];
+        if (blk != undefined) blk = blocks[this.col - 1][this.row];
+        break;
+      case 'R':
+        blk = blocks[this.col + 1];
+        if (blk != undefined) blk = blocks[this.col + 1][this.row];
+        break;
+      case 'U':
+        blk = blocks[this.col];
+        if (blk != undefined) blk = blocks[this.col][this.row + 1];
+        break;
+      case 'D':
+        blk = blocks[this.col];
+        if (blk != undefined) blk = blocks[this.col][this.row - 1];
+        break;
     }
-    return;
+
+    if (blk != undefined) return blk;
   }
 
   display() {
-    if (this.poped === false) {
+    if (this.poped == false) {
       fill(this.color);
       rect(this.x, this.y, this.width, this.height, 7);
-      // textSize(14);
-      // fill(0,0,0);
-      // text(`${this.idx}`, this.x+10, this.y+15);
-      
+
+      // fill(0);
+      // text(`${this.col}, ${this.row}`, this.x+this.width/4, this.y + this.height/2);
     }
   }
 }
